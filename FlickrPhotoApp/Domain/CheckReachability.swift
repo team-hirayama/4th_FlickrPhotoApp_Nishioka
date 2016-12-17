07257edee16a3952cfc2fd21609e44237c0926f8
takumi314 <isospin_symmetry@me.com>
2016-12-17 14:07:13 +0900

    add CheckReachability

---
+++ FlickrPhotoApp/Domain/CheckReachability.swift
@@ -0,0 +1,37 @@
+//
+//  CheckReachability.swift
+//  FlickrPhotoApp
+//
+//  Created by NishiokaKohei on 2016/12/17.
+//  Copyright © 2016年 Kohey. All rights reserved.
+//
+
+import SystemConfiguration
+import Foundation
+
+class CheckReachability: AnyObject {
+    
+    // MARK: - デバイスがオンラインかどうかを判定する。
+    // ネットワークへの接続が確立できない場合にはfalseを返す。
+    internal func checkReachability(hostName: String) -> Bool {
+        
+        let reachability = SCNetworkReachabilityCreateWithName(nil, hostName)!
+        var flags = SCNetworkReachabilityFlags.ConnectionAutomatic
+        
+        //Determines if the given target is reachable using the current network configuration.
+        guard SCNetworkReachabilityGetFlags(reachability, &flags) else {
+            return false
+        }
+        
+        return isReachable(flags) && !needsConnection(flags)
+    }
+    
+    private func isReachable(flags: SCNetworkReachabilityFlags) -> Bool {
+        return flags.rawValue & UInt32(kSCNetworkFlagsReachable) != 0
+    }
+    
+    private func needsConnection(flags: SCNetworkReachabilityFlags) -> Bool {
+        return flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired) != 0
+    }
+    
+}
