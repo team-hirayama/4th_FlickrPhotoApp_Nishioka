72c33edad784afeb8136fbb06d2a51358e3e1b12
takumi314 <isospin_symmetry@me.com>
2016-12-17 13:17:14 +0900

    First commit

---
+++ FlickrPhotoAppTests/FlickrPhotoAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  FlickrPhotoAppTests.swift
+//  FlickrPhotoAppTests
+//
+//  Created by NishiokaKohei on 2016/12/17.
+//  Copyright © 2016年 Kohey. All rights reserved.
+//
+
+import XCTest
+@testable import FlickrPhotoApp
+
+class FlickrPhotoAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+
+target 'FlickrPhotoApp' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FlickrPhotoApp
+
+  target 'FlickrPhotoAppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'FlickrPhotoAppUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
